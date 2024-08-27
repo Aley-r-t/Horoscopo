d52ad5c70c34e87c513a08cdf1f259e9382b8579
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-08-27 14:41:53 -0500

    Navegacion

--- app/src/main/java/com/version1/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/version1/horoscapp/ui/home/MainActivity.kt
@@ -5,26 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.version1.horoscapp.R
 import com.version1.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+
         enableEdgeToEdge()
-
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost :NavHostFragment = supportFragmentManager.findFragmentById(R.id.FragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
